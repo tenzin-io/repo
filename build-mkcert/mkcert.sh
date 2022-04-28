aa34fd3d8253ef44af1c06a95d6702f089984fa1
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2022-04-27 20:51:22 -0400

    Simplify mkcert container

--- build-mkcert/mkcert.sh
+++ build-mkcert/mkcert.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-test -d $CAROOT || mkdir $CAROOT
-test -e $CAROOT/$CERT_HOSTNAME.crt && exit 0
-
 mkcert -install
-mkcert -cert-file $CAROOT/$CERT_HOSTNAME.crt -key-file $CAROOT/$CERT_HOSTNAME.key $CERT_HOSTNAME.$CERT_DOMAIN
+mkcert -cert-file ./$CERT_HOSTNAME.crt -key-file ./$CERT_HOSTNAME.key $CERT_HOSTNAME.$CERT_DOMAIN
